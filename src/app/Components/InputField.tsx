627ef151efe96723f17e0e0e2eb55f3eeca2b332
SabaSaleem5317 <SabaSaleem509@gmail.com>
2025-08-01 16:00:46 +0500

    Integrated Prettier/Husky/Lint Staged

--- src/app/Components/InputField.tsx
+++ src/app/Components/InputField.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import TextField, { TextFieldProps } from "@mui/material/TextField";
+import React from 'react';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
 
 type InputFieldProps = TextFieldProps & {
   errorMessage?: string;
 };
 const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
-  ({ errorMessage, children,...props }, ref) => {
+  ({ errorMessage, children, ...props }, ref) => {
     return (
       <div className="mb-4">
-      <TextField
-        variant="outlined"
-        fullWidth
-        error={!!errorMessage}
-        helperText={errorMessage}
-        inputRef={ref}
-        {...props}
-      >
-       {props.select && children }
-      </TextField>
+        <TextField
+          variant="outlined"
+          fullWidth
+          error={!!errorMessage}
+          helperText={errorMessage}
+          inputRef={ref}
+          {...props}
+        >
+          {props.select && children}
+        </TextField>
       </div>
     );
   }
 );
-InputField.displayName = "InputField";
+InputField.displayName = 'InputField';
 export default InputField;
