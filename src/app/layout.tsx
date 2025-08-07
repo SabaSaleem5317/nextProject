d05a5a1a4a6b7aed9ba476fb872a8023a86cc400
SabaSaleem5317 <SabaSaleem509@gmail.com>
2025-08-07 14:51:16 +0500

    resolved error in layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import localFont from 'next/font/local';
 
-const geistSans = localFont({
-  src: './fonts/Geist-Regular.woff2',
+const geistSans = Geist({
   variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
-const geistMono = localFont({
-  src: './fonts/GeistMono-Regular.woff2',
+const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
